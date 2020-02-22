6c38e5a50613082ada87c2e50ff4f794d2e6aff2
Adam Yi <i@adamyi.com>
2020-02-22 15:35:49 +1100

    add course names to upcoming ddls

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A Chrome extension to enhance your WebCMS3 experience :)
 ## Features
 1. Dark Mode
 2. Add non-webcms3 courses to WebCMS3 navigation bar (by adding to WebCMS4 folder in Chrome bookmark)
+3. Add course names to "upcoming due dates" on front page
 
 More to be added.
 
--- contentScript.js
+++ contentScript.js
@@ -8,5 +8,21 @@ function dumpBookmarks() {
     }
   });
 }
-// dumpBookmarks();
-document.addEventListener('DOMContentLoaded', function() { dumpBookmarks(); });
+function refineDues() {
+  panels = document.getElementsByClassName("panel-title");
+  for (i = 0; i < panels.length; i++) {
+    if (panels[i].innerText == "Upcoming Due Dates") {
+      ddls = panels[i].parentElement.parentElement.getElementsByTagName("a");
+      for (j = 0; j < ddls.length; j++) {
+        course = ddls[j].href.substr(32);
+        course = course.substr(0, course.indexOf("/"));
+        ddls[j].innerHTML += "<span class=\"label\">" + course + "</span>"
+      }
+    }
+  }
+}
+document.addEventListener('DOMContentLoaded', function() {
+  dumpBookmarks();
+  if (window.location.href == "https://webcms3.cse.unsw.edu.au/")
+    refineDues();
+});
